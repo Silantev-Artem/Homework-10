eb303e1ce8d62cde884a80518e320d9b7ef78ca4
Silantev Artem <silantev.artem@yandex.ru>
2025-04-08 23:49:42 +0300

    Сделано дз №10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("\nЗадание 2");
+
+        String fullNameReport = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameReport);
+
+        System.out.println("\nЗадание 3");
+
+        String fullNameNew = "Иванов Семён Семёнович";
+        fullNameNew = fullNameNew.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNew);
+
+
     }
 }
